7317c3d9717ae964b6462dd1fa3cbbe72e330f97
Viktor Holovach <viktorgolovach@Viktors-MacBook-Pro.local>
2023-08-10 13:57:30 +0200

    initial commit

---
+++ MVP_Example/MVP_Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVP_Example
+//
+//  Created by Viktor Golovach on 10.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
